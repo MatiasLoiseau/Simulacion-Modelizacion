2e58cb4be5348a7de5aff3a08e4fb77feaf1f1ff
MatiasLoiseau <matiasloiseau@gmail.com>
2017-10-02 20:56:39 -0300

    Add first classes

---
+++ teoriaDeColas/src/sistema/Cronometro.java
@@ -0,0 +1,25 @@
+package sistema;
+
+public class Cronometro {
+	
+	//Variables
+	public int tiempoSimulacion;
+	
+	//Getters and setters
+	public int getTiempoSimulacion() {
+		return tiempoSimulacion;
+	}
+
+	public void setTiempoSimulacion(int tiempoSimulacion) {
+		this.tiempoSimulacion = tiempoSimulacion;
+	}
+
+	//funciones
+	public void inicializacion() {
+		setTiempoSimulacion(0);
+	}
+	public void iterador() {
+		
+	}
+	
+}
